e1e91bd6e9fc21b60b00495b8119fef47da22c5f
culitoricouwu-arch <culitoricouwu@gmail.com>
2026-07-03 22:35:28 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,24 +8,22 @@ WEBHOOK = os.environ["WEBHOOK"]
 with open("characters.json", encoding="utf-8") as f:
     data = json.load(f)
 
-chance = data["chance"]
-
-if random.randint(1,100) > chance:
-    print("Nadie habló hoy.")
+if random.randint(1,100) > data["chance"]:
+    print("Nadie habló.")
     quit()
 
-personaje = random.choice(data["characters"])
-
-mensaje = random.choice(personaje["messages"])
+personaje=random.choice(data["characters"])
+mensaje=random.choice(personaje["messages"])
 
 requests.post(
     WEBHOOK,
     json={
-        "username": personaje["name"],
-        "avatar_url": personaje["avatar"],
-        "content": mensaje
+        "username":personaje["name"],
+        "avatar_url":personaje["avatar"],
+        "content":mensaje
     }
 )
 
+print("Publicado:",personaje["name"])
 print(personaje["name"])
 print(mensaje)
